22646e2bfc7afb6d5033ebfb973b260fefc3681d
Can <Can@CAN>
2021-04-19 21:37:09 +0300

    Revert "06.01.2021 Ders Sonu"
    This reverts commit 118c07985369b4c009b7589c482a91d453c2e5be.

--- Business/Abstract/IProductService.cs
+++ Business/Abstract/IProductService.cs
@@ -1,5 +1,4 @@
 ﻿using Entities.Concrete;
-using Entities.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -11,6 +10,5 @@ namespace Business.Abstract
         List<Product> GetAll();
         List<Product> GetAllByCategoryId(int id);
         List<Product> GetByUnitPrice(decimal min,decimal max);
-        List<ProductDetailDto> GetProductDetails();
     }
 }
